edf4ca992ea06e640856dcabac7f2e840a735641
Wendell Xavier <wendellxavier0612@gmail.com>
2026-08-10 10:22:24 -0300

    alterando regex

--- trabalhando-com-string-e-regex/substituindo_palavras.py
+++ trabalhando-com-string-e-regex/substituindo_palavras.py
@@ -1,10 +1,8 @@
 import re
 
+texto = input("Digite o texto a ser revisado: ")  
+palavra_antiga = input("Qual palavra deseja substituir? ")  
+palavra_nova = input("Qual a nova palavra? ")  
 
-texto = input('Digite o texto a ser revisado: ').split()
-palavra_substituir = input('Digite a palavra que deseja substituir: ')
-palavra_nova = input(f'Digite a palavra que irá substituir a palavra {palavra_substituir}: ')
-
-lista = texto 
-
-print(lista)
+nova_frase = re.sub(rf'\b{palavra_antiga}\b', palavra_nova, texto)
+print(nova_frase)
